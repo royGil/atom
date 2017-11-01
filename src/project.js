9be9c0eadb06080ccf820896beb41fe25ee74509
hansonw <hansonw@fb.com>
2017-11-01 15:40:00 -0700

    Allow directory providers to implement onDidChangeFiles for custom pathwatchers

--- src/project.js
+++ src/project.js
@@ -338,13 +338,19 @@ class Project extends Model {
     }
 
     this.rootDirectories.push(directory)
-    this.watcherPromisesByPath[directory.getPath()] = watchPath(directory.getPath(), {}, events => {
+
+    const didChangeCallback = events => {
       // Stop event delivery immediately on removal of a rootDirectory, even if its watcher
       // promise has yet to resolve at the time of removal
       if (this.rootDirectories.includes(directory)) {
         this.emitter.emit('did-change-files', events)
       }
-    })
+    }
+    this.watcherPromisesByPath[directory.getPath()] =
+      // Allow directory providers to provide their own onDidChangeFiles callback.
+      directory.onDidChangeFiles != null ?
+        Promise.resolve(directory.onDidChangeFiles(didChangeCallback)) :
+        watchPath(directory.getPath(), {}, didChangeCallback);
 
     for (let watchedPath in this.watcherPromisesByPath) {
       if (!this.rootDirectories.find(dir => dir.getPath() === watchedPath)) {
